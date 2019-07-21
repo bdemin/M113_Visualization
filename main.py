d50d70ee08e0a5ee8e56fc8f10aba14e4d96f866
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-21 19:13:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from draw_system import draw_system
 from simulation_description import show_description
 
 
-path_directory = 'C:/Users/slavd/Documents/GitHub/M113_tests/Data_Movies/3d_Rocks1/'
-# path_directory = 'C:/Users/slavd/Documents/GitHub/M113_tests/Data_Movies/Simulation_5/'
+# path_directory = '../M113_tests/Data_Movies/3d_Rocks1/'
+path_directory = '../M113_tests/Data_Movies/Simulation_5/'
 show_description(path_directory)
 
 #%%create Chassis object:
