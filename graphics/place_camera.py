50974cf9819ad1a85f24f6fb5943fe7cd3c97c59
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-25 19:59:46 +0300

    Update place_camera - add view picker

--- graphics/place_camera.py
+++ graphics/place_camera.py
@@ -2,16 +2,26 @@ import numpy as np
 
 
 def place_camera(camera, chs_pos, chs_ang):
-    # define camera parameters:
-    cam_d = 14 #[m]
-    # cam_h = 5 #[m]
-    cam_h = 1
-    cam_angle = np.arcsin(cam_h/cam_d) # [rad]
-    cam_h = cam_d * np.sin(cam_angle)
-    
-    # chs2cam = [cam_d*np.sin(chs_ang[2]), -cam_d*np.cos(chs_ang[2]), cam_h]
-    chs2cam = [2 , -cam_d, cam_h]
-    # chs2cam = [-15,0,0]
+    # Define camera parameters
+    view = 3
+    if view == 1:
+        # General view
+        cam_d = 14 #[m]
+        cam_h = 5 #[m]
+        chs2cam = [2 , -cam_d, cam_h]
+
+    elif view == 2:
+        # Rear view
+        cam_d = 14
+        cam_h = 1
+        chs2cam = [-15,0,0]
+
+    elif view == 3:
+        # Wheel view
+        cam_d = 14 #[m]
+        cam_h = 0
+        chs2cam = [0,-2,0]
+
     camera_pos = chs_pos + chs2cam
 
     # place camera and set focal point:
