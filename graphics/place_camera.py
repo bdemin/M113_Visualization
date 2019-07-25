d5c6fcf34c6f8d892085a1222fdad138e41e1e81
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-25 20:15:43 +0300

    Updated place_camera

--- graphics/place_camera.py
+++ graphics/place_camera.py
@@ -9,21 +9,19 @@ def place_camera(camera, chs_pos, chs_ang):
         cam_d = 14 # [m]
         cam_h = 5 # [m]
         chs2cam = [2 , -cam_d, cam_h] # vector from chassis to camera position
+        camera_pos = chs_pos + chs2cam
 
     elif view == 2:
         # Rear view
-        cam_d = 14
-        cam_h = 1
-        chs2cam = [-15,0,0]
+        chs2cam = [-12,0,0]
+        camera_pos = chs_pos + chs2cam
 
     elif view == 3:
         # Wheel view
-        cam_d = 14
-        cam_h = 0
-        chs2cam = [0,-2,0]
-
-    camera_pos = chs_pos + chs2cam
-
+        chs2cam = [0,-5,0]
+        chs_fix = [0,0,-1]
+        camera_pos = chs_pos + chs2cam + chs_fix
+    
     # Place camera and set focal point:
     camera.SetViewUp([0,0,1])
     camera.SetPosition(camera_pos)
