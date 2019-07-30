fb430285f18fb00dbd6c0c447cca1718715175b8
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-30 17:13:15 +0300

    Add place_all_bodies method

--- graphics/place_object.py
+++ graphics/place_object.py
@@ -4,7 +4,17 @@ from vtk import vtkTransform, vtkMatrix4x4, vtkTransformPolyDataFilter
 
 from graphics.transformations import trans_matrix, rot_matrix
 
-    
+
+def place_all_bodies(data, timer_count):
+    for obj_index in range(len(data)):
+        for _object in data[obj_index]:
+            _object.Move(_object.path_loc[timer_count],
+                _object.path_dir[timer_count])
+            place_object(_object.actor,
+                _object.position,
+                _object.angles)    
+
+
 def place_object(actor, new_pos, angles):
     matrix = np.matmul(trans_matrix(*new_pos) , rot_matrix(*angles))
     trans = vtkTransform()
