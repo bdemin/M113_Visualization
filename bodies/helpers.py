e346de3708a43e73341b3fb691289ec6c4ab6c6d
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-31 15:47:33 +0300

    Update helpers.py

--- bodies/helpers.py
+++ bodies/helpers.py
@@ -16,7 +16,7 @@ def create_bodies(path_directory, type_, side = None):
             if index < num_cols/2:
                 # if path_loc[0][1] > 0:
                 side = 'L'
-                # path_dir[:,2] = path_dir[:,2] + np.pi
+                path_dir[:,2] = path_dir[:,2] + np.pi
             else:
                 side = 'R'
         bodies.append(Body(type_, path_loc, path_dir, side))    
