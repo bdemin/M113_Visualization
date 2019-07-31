7b79b13d80a56a3dc31a3d29b060f5cd2a98867e
bdemin <42113340+bdemin@users.noreply.github.com>
2019-07-31 15:47:41 +0300

    Update visualize.py

--- graphics/visualize.py
+++ graphics/visualize.py
@@ -8,7 +8,7 @@ from graphics.place_object import place_all_bodies
 from graphics.transformations import scale_actor
 from graphics.draw_text import draw_text
 
-from help_functions.event_handling import keyboard_events
+from helpers.event_handling import keyboard_events
 
 from bodies.classes import Surface
 
@@ -71,7 +71,7 @@ class vtkTimerCallback(object):
             print('Simulation End')
 
 
-def visualize(*args, path_directory, total_time = 25):
+def visualize(*args, directory, total_time = 25):
     # create renderer, figure and axes:
     renderer = vtkRenderer()
     renWin = vtkRenderWindow()
@@ -98,7 +98,7 @@ def visualize(*args, path_directory, total_time = 25):
         for obj in args[i]:
             renderer.AddActor(obj.actor)
 
-    surface = Surface(path_directory)
+    surface = Surface(directory)
     for actor in surface.actors:
         renderer.AddActor(actor)
         
