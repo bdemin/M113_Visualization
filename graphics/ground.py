883e0bc5615f7f727298a1718c4b3d73b724d8a4
bdemin <42113340+bdemin@users.noreply.github.com>
2019-08-06 21:08:12 +0300

    Assign soil type colors to the ground vtkPolyData

--- graphics/ground.py
+++ graphics/ground.py
@@ -16,7 +16,7 @@ def visualize_elevation(PolyData):
     colorLookupTable.SetTableRange(minz, maxz)
     colorLookupTable.Build()
 
-    # Generate the colors for each point based on the color map
+    # Generate the colors for each point based on elevation
     colors = vtkUnsignedCharArray()
     colors.SetNumberOfComponents(3)
     colors.SetName("Colors")
@@ -32,6 +32,8 @@ def visualize_elevation(PolyData):
             color[j] = int(255.0 * dcolor[j])
 
         colors.InsertNextTypedTuple(color)
+
+    # Assign colors to the vtkPolyData
     PolyData.GetPointData().SetScalars(colors)
 
 
@@ -45,15 +47,17 @@ def get_soil_color(soil_type_key, colors_dict):
 
 def visualize_soil(PolyData, soil_type_array):
     # Assign colors to the ground based on input soil type array
+
+    # Create a color dictionary
     colors = vtkNamedColors()
     colors_dict = {
-        0: colors.HTMLColorToRGB('SandyBrown'),
-        1: colors.HTMLColorToRGB('ForestGreen'),
-        2: colors.HTMLColorToRGB('Sienna'),
-        3: colors.HTMLColorToRGB('DarkOliveGreen'),
-        4: colors.HTMLColorToRGB('BurlyWood'),
-        5: colors.HTMLColorToRGB('RosyBrown'),
-        6: colors.HTMLColorToRGB('DarkKhaki')}
+        0: colors.HTMLColorToRGB('DarkKhaki'),
+        1: colors.HTMLColorToRGB('Wheat'),
+        2: colors.HTMLColorToRGB('BurlyWood'),
+        3: colors.HTMLColorToRGB('SandyBrown'),
+        4: colors.HTMLColorToRGB('Khaki'),
+        5: colors.HTMLColorToRGB('DarkOliveGreen'),
+        6: colors.HTMLColorToRGB('DarkGreen')}
 
     m, n = soil_type_array.shape
     soil_color_map = vtkUnsignedCharArray()
@@ -63,18 +67,20 @@ def visualize_soil(PolyData, soil_type_array):
     for i in range(m-1):
         for j in range(n-1):
             for _ in range(6):
-                soil_type = soil_type_array[i,j]
-                soil_color_map.InsertNextTypedTuple(get_soil_color(soil_type, colors_dict))
+                soil_color_map.InsertNextTypedTuple(get_soil_color(soil_type_array[i,j], colors_dict))
 
+    # Assign colors to the vtkPolyData
     PolyData.GetPointData().SetScalars(soil_color_map)
 
 
-def generate_perlin_noise_2d(shape, res):
+def generate_perlin_noise_2d(size, res):
+    # Return Perlin Noise
+    
     def f(t):
         return 6*t**5 - 15*t**4 + 10*t**3
     
-    delta = (res[0] / shape[0], res[1] / shape[1])
-    d = (shape[0] // res[0], shape[1] // res[1])
+    delta = (res[0] / size[0], res[1] / size[1])
+    d = (size[0] // res[0], size[1] // res[1])
     grid = np.mgrid[0:res[0]:delta[0],0:res[1]:delta[1]].transpose(1, 2, 0) % 1
     # Gradients
     angles = 2*np.pi*np.random.rand(res[0]+1, res[1]+1)
@@ -96,21 +102,27 @@ def generate_perlin_noise_2d(shape, res):
 
 
 def create_soil_type_arr(size):
-    noise = generate_perlin_noise_2d(size, (139, 3))
-    for i in range(size[0]):
-        for j in range(size[1]):
-            if noise[i,j] < -0.2:
-                noise[i,j] = 0
-            elif noise[i,j] < 0.2:
-                noise[i,j] = 1
-            elif noise[i,j] < 0.4:
-                noise[i,j] = 2
-            elif noise[i,j] < 0.6:
-                noise[i,j] = 3
-            elif noise[i,j] < 0.7:
-                noise[i,j] = 4
-            elif noise[i,j] < 0.8:
-                noise[i,j] = 5
-            else:
-                noise[i,j] = 6
-    return noise
+    # Create an array of different soil types scatterd randomly using Perlin Noise
+
+    np.random.seed(1)
+    noise = generate_perlin_noise_2d(size, (139, 6)) # Get perlin noise
+    soil_type_arr = np.zeros(size)
+
+    num_soil_types = 2 # Define how many soil types (colors) should be set
+    arr_min = noise.min()
+    soil_type_step = (noise.max() - arr_min)/num_soil_types
+    # Fill array with different soil types
+    for k in range(num_soil_types):
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if (noise[i,j] <= arr_min + (k+1)*soil_type_step) and (arr_min + k*soil_type_step <= noise[i,j]):
+                    soil_type_arr[i,j] = k
+
+    # np.place(noise, noise < (arr_min + soil_type_step), 0)
+    # np.place(noise, (arr_min + soil_type_step < noise) * (noise < arr_min + 2*soil_type_step), 1)
+    # np.place(noise, (arr_min + 2*soil_type_step < noise) * (noise < arr_min + 3*soil_type_step), 2)
+    # np.place(noise, (arr_min + 3*soil_type_step < noise) * (noise < arr_min + 4*soil_type_step), 3)
+    # np.place(noise, (arr_min + 4*soil_type_step < noise) * (noise < arr_min + 5*soil_type_step), 4)
+    # np.place(noise, (arr_min + 5*soil_type_step < noise) * (noise < arr_min + 6*soil_type_step), 5)
+    # np.place(noise, noise.max() - soil_type_step < noise, 6)
+    return soil_type_arr
