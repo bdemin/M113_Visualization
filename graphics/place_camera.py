f4e5acb32a8207d1f8a1cfcb9261ef8537bf5de5
bdemin <42113340+bdemin@users.noreply.github.com>
2019-08-20 15:55:11 +0300

    Changed some camera parameters and added Front View camera option

--- graphics/place_camera.py
+++ graphics/place_camera.py
@@ -7,8 +7,8 @@ def place_camera(time, data, camera, camera_distance, view):
     if view == 1:
         # General view
         chs_pos = data[0][0].path_loc[time] # Chassis CG @ time
-        cam_d = 14 # [m]
-        cam_h = 5 # [m]
+        cam_d = 12 # [m]
+        cam_h = 4.5 # [m]
         chs2cam = [2 , -cam_d, cam_h] # vector from chassis to camera position
         chs_fix = [0,0,0]
         camera_pos = chs_pos + chs2cam
@@ -27,7 +27,15 @@ def place_camera(time, data, camera, camera_distance, view):
         # Wheel view
         wheel_pos = data[1][7].path_loc[time] # Wheel #7 CG @ time
         cam_focal_point = wheel_pos
-        camera_pos = wheel_pos + [0,-3,0.5]
+        camera_pos = wheel_pos + [0,-1,0.4]
+
+    elif view == 4:
+        # Rear view
+        chs_pos = data[0][0].path_loc[time] # Chassis CG @ time
+        chs2cam = [7,0,-0.5]
+        camera_pos = chs_pos + chs2cam
+
+        cam_focal_point = chs_pos
     
     # Place camera and set focal point:
     camera.SetViewUp([0,0,1])
