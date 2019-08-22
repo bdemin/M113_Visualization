9f1dfd8fea8aa0019faf0ef401405472c2750e7e
bdemin <42113340+bdemin@users.noreply.github.com>
2019-08-22 17:43:36 +0300

    Update functions.py

--- helpers/functions.py
+++ helpers/functions.py
@@ -17,5 +17,6 @@ def get_directory(path):
     from os import listdir
     folder_list = listdir(path)
     folder_list = sorted([folder for folder in folder_list])
-
+    
+    print(folder_list[-1])
     return path + folder_list[-1] + '/'
