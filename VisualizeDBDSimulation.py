56425f99a8228f824bbd0db3cf70853f4fc54cbe
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-03 20:32:12 +0200

    Added surface creation and some visualization-related methods

--- VisualizeDBDSimulation.py
+++ VisualizeDBDSimulation.py
@@ -5,7 +5,7 @@ import bodies.vehicle
 
 from surface.classes import Surface
 
-# from graphics.visualize2 import visualize
+from visualization.visualizer import Visualizer
 
 # from simulation_description import show_description
 
@@ -14,14 +14,23 @@ class VisualizeDBDSimulation(object):
     def __init__(self, visualization_params):
         self.params = visualization_params
         self.path = get_latest_directory(visualization_params['parent_directory'])
-        self.total_time = np.loadtxt(self.path + 'Time_Data.txt', delimiter = ',')
 
 
     def load_bodies_data(self):
         # Need to add bodies param
         invoke_vehicle_method = getattr(bodies.vehicle, self.params['vehicle_type'])
-        self.bodies = invoke_vehicle_method(self.path)
+        self.vehicle = invoke_vehicle_method(self.path)
 
 
     def load_surface_data(self):
-        self.surface = Surface(self.path, self.params['surface'])
+        self.surface = Surface(self.path, None, self.params['surface'], self.vehicle.bodies['Chassis'][0].path_loc)
+    
+
+    def load_visualization(self):
+        total_time = np.loadtxt(self.path + 'Time_Data.txt', delimiter = ',')
+        num_frames = self.vehicle.bodies['Chassis'][0].path_dir.shape[0]
+
+        visualizer = Visualizer(self.params['video'])
+        visualizer.add_actors(self.vehicle.bodies, self.surface)
+        visualizer.init_callback(self.vehicle.bodies, total_time, num_frames)
+        
