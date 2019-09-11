60e1a8c6226bb67878b5f9c06e4ad8510eed2a92
bdemin <42113340+bdemin@users.noreply.github.com>
2019-09-11 15:00:13 +0300

    Visualization parameters are now loaded from a class: VisualizationParameters

--- helpers/get_visualization_params.py
+++ helpers/get_visualization_params.py
@@ -1,36 +1,88 @@
-def m113_novid():
-    # M113 visualization with all bodies, surface and no video recording
+class VisualizationParameters(object):
+    # Class that deals with various visualization parameters - which bodies to include, surface filters, video recording, etc.
+    
+    # def __init__(self):
+        # pass
 
-    parent_directory = '../M113_tests/Data_Movies/'
 
-    vehicle_type = 'M113'
+    def load_data(self, method_string):
+        invoke_method = getattr(self, method_string)
+        invoke_method()
 
-    bodies = {
-        'Chassis': True,
-        'road_wheels': True,
-        'trailing_arms': True,
-        'sprockets': True,
-        'idlers': True,
-        'track_units': True
-    }
 
-    surface = {
-        'visualize_surface': True,
-        'soil_map_flag': True,
-        'color_map_flag': False,
-        'path_spline_flag': True
-    }
+    def get_data(self):
+        # Return all parameters regarding the visualization
+        return self.params
 
-    video = {
-        'record_video_flag': False,
-    }
 
-    visualization_params = {
-        'vehicle_type': vehicle_type,
-        'parent_directory': parent_directory,
-        'bodies': bodies,
-        'surface': surface,
-        'video': video
-    }
+    def m113_novid(self):
+        # M113 visualization with all bodies, surface and no video recording
 
-    return visualization_params
+        parent_directory = '../M113_tests/Data_Movies/'
+
+        vehicle_type = 'M113'
+
+        bodies = {
+            'Chassis': True,
+            'road_wheels': True,
+            'trailing_arms': True,
+            'sprockets': True,
+            'idlers': True,
+            'track_units': True
+        }
+
+        surface = {
+            'visualize_surface': True,
+            'soil_map_flag': True,
+            'color_map_flag': False,
+            'path_spline_flag': True
+        }
+
+        video = {
+            'record_video_flag': False,
+        }
+
+        self.params = {
+            'vehicle_type': vehicle_type,
+            'parent_directory': parent_directory,
+            'bodies': bodies,
+            'surface': surface,
+            'video': video
+        }
+        
+
+    def m113_vid(self):
+        # M113 visualization with all bodies, surface and video recording
+
+        parent_directory = '../M113_tests/Data_Movies/'
+
+        vehicle_type = 'M113'
+
+        bodies = {
+            'Chassis': True,
+            'road_wheels': True,
+            'trailing_arms': True,
+            'sprockets': True,
+            'idlers': True,
+            'track_units': True
+        }
+
+        surface = {
+            'visualize_surface': True,
+            'soil_map_flag': True,
+            'color_map_flag': False,
+            'path_spline_flag': True
+        }
+
+        video = {
+            'record_video_flag': True,
+        }
+
+        self.params = {
+            'vehicle_type': vehicle_type,
+            'parent_directory': parent_directory,
+            'bodies': bodies,
+            'surface': surface,
+            'video': video
+        }
+        
