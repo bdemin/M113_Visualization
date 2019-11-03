8d528dfe6b2767526016c86f8d302ca7f27405a7
bdemin <42113340+bdemin@users.noreply.github.com>
2019-11-03 23:13:01 +0200

    Move create_bodies func

--- bodies/vehicle.py
+++ bodies/vehicle.py
@@ -1,15 +1,19 @@
 import numpy as np
 
+from bodies.classes import Body
 
 class Vehicle(object):
     # Metaclass to define common attributes of DBD vehicles.
+    
     def __init__(self, path):
         self.bodies = dict()
         self.bodies['Chassis'] = create_bodies(path, 'Chassis')
+        # can be improved (inputs, func rather than method, path, etc)
 
 
 class M113(Vehicle):
     # Inherits from Vehicle and adds all the relevant attributes for visualizing M113 DBD results.
+    
     def __init__(self, path):
         Vehicle.__init__(self, path)
         self.bodies['road_wheels'] = create_bodies(path, 'Road_Wheel', side = True)
@@ -32,3 +36,25 @@ class MK4(Vehicle):
 class D9(Vehicle):
     def __init__(self):
         Vehicle.__init__(self)
+        
+
+def create_bodies(path_directory, type_, side = None):
+    # Return list of similar bodies
+
+    bodies = []
+    path_data = np.loadtxt(path_directory + type_ + '.txt', delimiter = ',')
+    num_cols = int(path_data.shape[1])
+    for index in range(0, num_cols, 6):
+        loc_slice = slice(index, index+3)
+        dir_slice = slice(index+3, index+6)
+        path_loc = np.copy(path_data[:, loc_slice])
+        path_dir = np.copy(path_data[:, dir_slice])
+        if side:
+            if index < num_cols/2:
+                side = 'L'
+                path_dir[:,2] = path_dir[:,2] - np.pi
+                path_dir[:,0] = -path_dir[:,0]
+            else:
+                side = 'R'
+        bodies.append(Body(type_, path_loc, path_dir, side))
+    return bodies
