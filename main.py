aab4d6b018ee922c12960b8be022141e8f0711cf
bdemin <42113340+bdemin@users.noreply.github.com>
2019-11-14 22:28:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from simulation_description import show_description
 
 
 path = '../M113_tests/Data_Movies/'
-directory = get_directory(path)
+# directory = path + 'Step/'
+directory = path + 'Slope/'
 
 show_description(directory)
 
