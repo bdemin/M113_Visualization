fd971d758574ea4212b9528c4541d8653af14cb6
bdemin <42113340+bdemin@users.noreply.github.com>
2019-11-17 20:49:43 +0200

    Commented out Delauny method (to be later tested)

--- surface/classes.py
+++ surface/classes.py
@@ -61,34 +61,34 @@ class Surface(object):
         
         # Build the meshgrid (need to try Delauney)
 
-        profile = vtk.vtkPolyData()
-        profile.SetPoints(points)
+        # profile = vtk.vtkPolyData()
+        # profile.SetPoints(points)
 
         # Delaunay3D is used to triangulate the points. The Tolerance is the
         # distance that nearly coincident points are merged
         # together. (Delaunay does better if points are well spaced.) The
         # alpha value is the radius of circumcircles, circumspheres. Any mesh
         # entity whose circumcircle is smaller than this value is output.
-        from vtk import vtkDelaunay3D
-        delny = vtkDelaunay3D()
-        delny.SetInputData(profile)
-        delny.SetTolerance(0.01)
-        delny.SetAlpha(0.2)
-        delny.BoundingTriangulationOff()
+        # from vtk import vtkDelaunay3D
+        # delny = vtkDelaunay3D()
+        # delny.SetInputData(profile)
+        # delny.SetTolerance(0.01)
+        # delny.SetAlpha(0.2)
+        # delny.BoundingTriangulationOff()
 
         # Shrink the result to help see it better.
-        shrink = vtk.vtkShrinkFilter()
-        shrink.SetInputConnection(delny.GetOutputPort())
-        shrink.SetShrinkFactor(0.9)
+        # shrink = vtk.vtkShrinkFilter()
+        # shrink.SetInputConnection(delny.GetOutputPort())
+        # shrink.SetShrinkFactor(0.9)
 
-        map = vtk.vtkDataSetMapper()
-        map.SetInputConnection(shrink.GetOutputPort())
+        # map = vtk.vtkDataSetMapper()
+        # map.SetInputConnection(shrink.GetOutputPort())
 
-        triangulation = vtk.vtkActor()
-        triangulation.SetMapper(map)
-        triangulation.GetProperty().SetColor(1, 0, 0)
+        # triangulation = vtk.vtkActor()
+        # triangulation.SetMapper(map)
+        # triangulation.GetProperty().SetColor(1, 0, 0)
 
-        ren.AddActor(triangulation)
+        # ren.AddActor(triangulation)
 
 
 
