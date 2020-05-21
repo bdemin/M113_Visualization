9c15b6b2e5f750adacb4c927e6033e27dd141127
bdemin <42113340+bdemin@users.noreply.github.com>
2020-05-21 23:31:35 +0300

    Fix chassis key to lowercase

--- src/visualizer/graphics/place_camera.py
+++ src/visualizer/graphics/place_camera.py
@@ -7,13 +7,14 @@ def place_camera(time, data, camera, camera_distance, view, slope = 0):
     if view == 'isometric':
         camera.SetViewUp([0,0,1])
 
+        chs_pos = data['chassis'][0].path_loc[time] # Chassis CG @ time
 
         # camera.SetRoll(slope)
         # camera.SetRoll(31)
 
     elif view == 2:
         # General view
-        chs_pos = data['Chassis'][0].path_loc[time] # Chassis CG @ time
+        chs_pos = data['chassis'][0].path_loc[time] # Chassis CG @ time
         cam_d = 12 # [m]
         cam_h = 4.5 # [m]
         chs2cam = [2 , -cam_d, cam_h] # vector from chassis to camera position
@@ -24,10 +25,11 @@ def place_camera(time, data, camera, camera_distance, view, slope = 0):
 
     elif view == 3:
         # Rear view
-        chs_pos = data['Chassis'][0].path_loc[time] # Chassis CG @ time
+        chassis_pos = data['chassis'][0].path_loc[time] # Chassis CG @ time
         chs2cam = [-9,0,3]
         # camera_pos = chassis_pos + chs2cam
 
+        chassis_dir = data['chassis'][0].path_dir[time]
         cam_d = 10
         # camera_pos = chassis_pos + [-cam_d*np.cos(chassis_dir[2]), -cam_d*np.sin(chassis_dir[2]), cam_d*np.sin(chassis_dir[1]) + 2.5]
         camera_pos = chassis_pos + chs2cam
@@ -40,6 +42,7 @@ def place_camera(time, data, camera, camera_distance, view, slope = 0):
         # For turning simulation
         camera.SetViewUp([0,0,1])
 
+        chs_pos = data['chassis'][0].path_loc[time] # Chassis CG @ time
         # chs2cam = [0 , -20, 5] # vector from chassis to camera position
         chs2cam = [14 , -20, 5] # vector from chassis to camera position
         camera_pos = chs_pos + chs2cam
@@ -58,6 +61,7 @@ def place_camera(time, data, camera, camera_distance, view, slope = 0):
         camera.SetViewUp([0,0,1])
 
         # cam_focal_point = data[0][0].path_loc[-1] + [-2,0,0]
+        cam_focal_point = data['chassis'][0].path_loc[-1] + [-7,0,0]
         # camera_pos = cam_focal_point + [-1.5,-15,0]
         camera_pos = cam_focal_point + [-4 * 1.3,-15 * 1.3,0]
         camera.SetDistance(20)
