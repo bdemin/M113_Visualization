0057da6caa7a108dc7e0e2830b6a6357a93b7b17
bdemin <42113340+bdemin@users.noreply.github.com>
2020-05-27 16:35:19 +0300

    Update constructor

--- src/visualizer/camera.py
+++ src/visualizer/camera.py
@@ -4,12 +4,13 @@ from vtk import vtkCamera
 class Camera(vtkCamera):
     # Camera class for controlling various scene views
 
-    def __init__(self, distance = 14, view = 'isometric'):
+    def __init__(self, distance = 20, view = 'isometric'):
         super().__init__()
         self.current_view = view
         self.distance = distance
-        self.slope = 0
         self.is_on = True
+        self.roll_angle = 0
+        self.dolly_factor = 1
         
     def place_camera(self, frame, chassis_pos_and_dir):
 
