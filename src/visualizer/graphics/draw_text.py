ac17a1cef72613e3aafa48ea49ec1f49bc4b3e1a
bdemin <42113340+bdemin@users.noreply.github.com>
2020-06-04 14:17:11 +0300

    Update text style

--- src/visualizer/graphics/draw_text.py
+++ src/visualizer/graphics/draw_text.py
@@ -9,9 +9,9 @@ class Text(object):
         self.actor = vtkTextActor()
         text_prop = self.actor.GetTextProperty()
         text_prop.SetFontFamilyToArial()
-        text_prop.SetFontSize(34)
+        text_prop.SetFontSize(28)
         text_prop.SetColor(1,1,1)
-        self.actor.SetDisplayPosition(80,600)
+        self.actor.SetDisplayPosition(40,800)
 
         self.actor.SetInput('Visualization ready')
 
